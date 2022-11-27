7d81076e76b51a1e40a1055a269de4775e4ccd46
Pavel <nikpa123@yandex.ru>
2022-11-27 15:57:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+string[] GetArrayFromString(string stringArray)
+{
+    string[] array = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return array;
+}
+
+Console.Clear();
+Console.WriteLine("Заполните массив через пробел: ");
+string input = Console.ReadLine()!;
+string[] array = GetArrayFromString(input);
+Console.WriteLine(String.Join(", ", array));
